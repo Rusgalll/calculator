c6e2aff27df4d2622ae87dbe336efaf0ac886f42
Ruslan Galimullin <ruslan.gali.galimullin@yandex.ru>
2021-07-30 13:20:03 +0300

    lesson02

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,21 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 100000;
+let income = 'Фриланс';
+let addExpenses = 'интернет, такси, коммунальные услуги';
+let deposit = true;
+let mission = 2000000;
+let period = 12;
 
-alert('Привет');
-console.log('Привет');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен '+ period + ' месяцев');
+console.log('Цель зарабоать ' + mission + ' рублей');
+
+addExpenses = addExpenses.toLowerCase().split('');
+console.log(addExpenses);
+
+budgetDay = money /30;
+console.log(budgetDay)
